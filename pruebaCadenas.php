1272fb3c0c89b961a7cce7b49789630072a4c493
PGallardB <96648165+PGallardB@users.noreply.github.com>
2021-12-25 10:36:34 +0100

    Add files via upload

---
+++ pruebaCadenas.php
@@ -0,0 +1,15 @@
+<?php
+
+/* Crear un proyecto php llamado pruebaCadenas. En dicho proyecto crear un programa php
+en el que tengamos 4 variables que almacenen: dni, apellidos, nombre, fecha de nacimiento. El
+programa deberá mostrar una cadena de texto con la siguiente información:
+El alumno (nombre y apellidos) nació el día (fecha), y tiene como dni (dni). */
+
+$dni="12345678K";
+$apellidos="Gomez Martinez";
+$nombre="Juan";
+$fecha="12-01-2021";
+
+echo "El alumno $nombre $apellidos nació el día $fecha , y tiene como dni $dni . ";
+
+?>
